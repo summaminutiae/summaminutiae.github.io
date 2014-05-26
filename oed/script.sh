6b952fd3b364f09eff75e20633ea703db3050972
Bill White <minutiae@gmail.com>
2014-05-25 19:35:13 -0500

    Site updated at 2014-05-26 00:35:13 UTC

---
+++ oed/script.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/bash
+cp V05.markdown V06a.markdown
+cp V05.markdown V06b.markdown
+cp V05.markdown V07.markdown
+cp V05.markdown V08a.markdown
+cp V05.markdown V08b.markdown
+cp V05.markdown V09a.markdown
+cp V05.markdown V09b.markdown
+cp V05.markdown V10a.markdown
+cp V05.markdown V10b.markdown
